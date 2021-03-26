33e9dded72309878ee91574abb743d626c96ae4a
nguyenthanhliem-ntl <nguyenthanhlimpk@gmail.com>
2021-03-26 13:52:55 +0700

    1

---
+++ thong_tin.js
@@ -0,0 +1,8 @@
+
+let ap = new Meo(5,'cái',5.3,'lp','5tr3'," f");
+document.write(ap.ggender()+'</br>')
+document.write(ap.aage()+'</br>')
+document.write(ap.wweight()+'</br>')
+document.write(ap.pprice()+'</br>')
+document.write(ap.ggeneric()+'</br>')
+document.write(ap.ddescription()+'</br>')
